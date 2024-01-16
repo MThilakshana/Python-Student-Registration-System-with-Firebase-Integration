22bc35005d426c238c10dde8e2501c6f24611693
MThilakshana <99790936+MThilakshana@users.noreply.github.com>
2024-01-16 22:55:50 +0530

    Add files via upload

--- program.py
+++ program.py
@@ -1,4 +1,95 @@
 from tkinter import *
+import pyrebase
+from tkinter import messagebox
+
+#connect to the firebae
+config = {
+    "apiKey": "AIzaSyAC-wVs1ByMhmK-xRYYCg6_uPtPilf-Ek4",
+    "authDomain": "studentregistration-89577.firebaseapp.com",
+    "projectId": "studentregistration-89577",
+    "databaseURL":"https://studentregistration-89577-default-rtdb.firebaseio.com/",
+    "storageBucket": "studentregistration-89577.appspot.com",
+    "messagingSenderId": "903546180662",
+    "appId": "1:903546180662:web:e23983e5c9a5bd8f0e3497",
+    "measurementId": "G-SWRQ508XDQ"
+}
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+
+#save data 
+def savedata():
+    idV = id.get()
+    nameV = name.get()
+    emailV = email.get()
+    departV = department.get()
+    facV = faculty.get()
+    uniV = university.get()
+    
+    if (idV=="" or nameV=="" or emailV=="" or departV=="" or facV=="" or uniV==""):
+        messagebox.showinfo("Warning","All fields required!")
+    else:
+        data = {"ID":idV,"Name":nameV,"Email":emailV,"Department":departV,"Faculty":facV,"University":uniV}
+        
+        database.child("Students").child(idV).set(data)
+        messagebox.showinfo("Message","Data saved successfully!")
+        id.delete(0,END)
+        name.delete(0,END)
+        email.delete(0,END)
+        department.delete(0,END)
+        faculty.delete(0,END)
+        university.delete(0,END)
+        
+#search data
+def searchdata():
+    idV = id.get()
+    if (idV==""):
+        messagebox.showinfo("Warning","Enter ID")
+    else:
+        dataVal = database.child("Students").child(idV).get().val()
+        name.insert(0,dataVal.get('Name'))
+        email.insert(0,dataVal.get('Email'))
+        department.insert(0,dataVal.get('Department'))
+        faculty.insert(0,dataVal.get('Faculty'))
+        university.insert(0,dataVal.get('University'))
+        
+#update Data
+def updateData():
+    idV = id.get()
+    nameV = name.get()
+    emailV = email.get()
+    departV = department.get()
+    facV = faculty.get()
+    uniV = university.get()
+        
+    if (idV=="" or nameV=="" or emailV=="" or departV=="" or facV=="" or uniV==""):
+        messagebox.showinfo("Warning","All fields required!")
+    else:
+        data = {"ID":idV,"Name":nameV,"Email":emailV,"Department":departV,"Faculty":facV,"University":uniV}
+        database.child("Students").child(idV).update(data)
+        messagebox.showinfo("Message","Data Updated Successfully!")
+        id.delete(0,END)
+        name.delete(0,END)
+        email.delete(0,END)
+        department.delete(0,END)
+        faculty.delete(0,END)
+        university.delete(0,END)
+        
+#delete btn
+def deleteData():
+    idV = id.get()
+    if (idV==""):
+        messagebox.showinfo("Warning","Enter ID")
+    else:
+        database.child("Students").child(idV).remove()
+        messagebox.showinfo("Message","Data Deleted Successfully!")
+        id.delete(0,END)
+        name.delete(0,END)
+        email.delete(0,END)
+        department.delete(0,END)
+        faculty.delete(0,END)
+        university.delete(0,END)
+        
 
 root=Tk()
 root.title('Student Registration')
@@ -22,7 +113,8 @@ id = Entry(root,
       font=('Times',12),
       fg='Black',
       bg='white',
-      width=40).place(x=200,y=70)
+      width=40)
+id.place(x=200,y=70)
 
 Label(root,
       text="Student Name",
@@ -34,7 +126,8 @@ name = Entry(root,
       font=('Times',12),
       fg='Black',
       bg='white',
-      width=40).place(x=200,y=110)
+      width=40)
+name.place(x=200,y=110)
 
 Label(root,
       text="E-mail",
@@ -46,7 +139,8 @@ email = Entry(root,
       font=('Times',12),
       fg='Black',
       bg='white',
-      width=40).place(x=200,y=150)
+      width=40)
+email.place(x=200,y=150)
 
 Label(root,
       text="Department",
@@ -58,7 +152,8 @@ department = Entry(root,
       font=('Times',12),
       fg='Black',
       bg='white',
-      width=40).place(x=200,y=190)
+      width=40)
+department.place(x=200,y=190)
 
 Label(root,
       text="Faculty",
@@ -70,7 +165,8 @@ faculty = Entry(root,
       font=('Times',12),
       fg='Black',
       bg='white',
-      width=40).place(x=200,y=230)
+      width=40)
+faculty.place(x=200,y=230)
 
 Label(root,
       text="University",
@@ -82,7 +178,8 @@ university = Entry(root,
       font=('Times',12),
       fg='Black',
       bg='white',
-      width=40).place(x=200,y=270)
+      width=40)
+university.place(x=200,y=270)
 
 searchbtn = Button(root,
                    text="Search",
@@ -91,7 +188,8 @@ searchbtn = Button(root,
                    bg="White",
                    width=15,
                    border=3,
-                   cursor="hand2").place(x=600,y=70)
+                   cursor="hand2",
+                   command=searchdata).place(x=600,y=70)
 
 updatebtn = Button(root,
                    text="Update",
@@ -100,7 +198,8 @@ updatebtn = Button(root,
                    bg="White",
                    width=15,
                    border=3,
-                   cursor="hand2").place(x=600,y=120)
+                   cursor="hand2",
+                   command=updateData).place(x=600,y=120)
 
 savebtn = Button(root,
                    text="Save",
@@ -109,7 +208,18 @@ savebtn = Button(root,
                    bg="White",
                    width=15,
                    border=3,
-                   cursor="hand2").place(x=600,y=170)
+                   cursor="hand2",
+                   command=savedata).place(x=600,y=170)
+
+deletebtn = Button(root,
+                   text="Delete",
+                   font=('Times',12),
+                   fg="Black",
+                   bg="White",
+                   width=15,
+                   border=3,
+                   cursor="hand2",
+                   command=deleteData).place(x=600,y=220)
 
 exitbtn = Button(root,
                    text="Exit",
@@ -119,7 +229,7 @@ exitbtn = Button(root,
                    width=15,
                    border=3,
                    cursor="hand2",
-                   command=root.destroy).place(x=600,y=260)
+                   command=root.destroy).place(x=600,y=270)
 
 
 root.mainloop()
